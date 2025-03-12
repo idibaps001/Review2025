9f52450138e78e789a8c89b1ddeb8636568b3917
Lu Han <118617010+idibaps001@users.noreply.github.com>
2025-03-12 17:28:00 +0100

    Create 01.Integrate_11_atherosclerotic_datasets.R

---
+++ 01.Atherosclerosis/06.Integration/01.Integrate_11_atherosclerotic_datasets.R
@@ -0,0 +1,78 @@
+#Integrate 11 datasets about atherosclerotic
+library(Seurat)
+library(tidyverse)
+library(celda)
+library(sctransform)
+library(data.table)
+library(cluster)
+#remotes::install_github('satijalab/seurat-wrappers')
+library(SeuratWrappers)
+library(patchwork)
+options(future.globals.maxSize = 3e10)
+setwd("/Users/lucia/Documents/34.immunometabolism/data")
+
+#Load 11 datasets
+coronary.monomac <- readRDS("Wirka_coronary_human/Wirka_processed_SCTtransformed_monomac.rds")
+rpe004_seurat_sct.monomac <- readRDS("Pan_carotid_human/pan_rpe004_processed_SCTtransformed_monomac.rds")
+rpe005_seurat_sct.monomac <- readRDS("Pan_carotid_human/pan_rpe005_processed_SCTtransformed_monomac.rds")
+rpe006_seurat_sct.monomac <- readRDS("Pan_carotid_human/pan_rpe006_processed_SCTtransformed_monomac.rds")
+ac_p1_seurat_sct.monomac <- readRDS("Alsaigh_carotid_human/alsaigh_ac_p1_processed_SCTtransformed_monomac.rds")
+pa_p1_seurat_sct.monomac <- readRDS("Alsaigh_carotid_human/alsaigh_pa_p1_processed_SCTtransformed_monomac.rds")
+ac_p2_seurat_sct.monomac <- readRDS("Alsaigh_carotid_human/alsaigh_ac_p2_processed_SCTtransformed_monomac.rds")
+pa_p2_seurat_sct.monomac <- readRDS("Alsaigh_carotid_human/alsaigh_pa_p2_processed_SCTtransformed_monomac.rds")
+ac_p3_seurat_sct.monomac <- readRDS("Alsaigh_carotid_human/alsaigh_ac_p3_processed_SCTtransformed_monomac.rds")
+pa_p3_seurat_sct.monomac <- readRDS("Alsaigh_carotid_human/alsaigh_pa_p3_processed_SCTtransformed_monomac.rds")
+ascending_aortic_n6.monomac <- readRDS("Pan_normal_human/Pan_ascending_aortic_n6_processed_SCTtransformed_monomac.rds")
+
+#Merge datasets
+merged_obj <- merge(x = coronary.monomac, y = list(rpe004_seurat_sct.monomac,rpe005_seurat_sct.monomac,
+                                                   rpe006_seurat_sct.monomac,ac_p1_seurat_sct.monomac,
+                                                   pa_p1_seurat_sct.monomac, ac_p2_seurat_sct.monomac,
+                                                   pa_p2_seurat_sct.monomac, ac_p3_seurat_sct.monomac, 
+                                                   pa_p3_seurat_sct.monomac, ascending_aortic_n6.monomac      ))
+
+merged_obj <- SCTransform(merged_obj, method = "glmGamPoi", 
+                          vars.to.regress = "percent.mt")
+merged_obj <- RunPCA(merged_obj, npcs = 30, verbose = F)
+
+#Do integration for SCTtransformed datasets
+Integration_atherosclerotic <- IntegrateLayers(
+  object = merged_obj,
+  method = RPCAIntegration,
+  normalization.method = "SCT",
+  verbose = F
+)
+
+Integration_atherosclerotic <- FindNeighbors(Integration_atherosclerotic, dims = 1:30, reduction = "integrated.dr")
+Integration_atherosclerotic <- FindClusters(Integration_atherosclerotic, resolution = 2)
+Integration_atherosclerotic <- RunUMAP(Integration_atherosclerotic, dims = 1:30, reduction = "integrated.dr")
+# Visualization
+DimPlot(Integration_atherosclerotic, reduction = "umap", group.by = "Tissue")
+
+#Integration_atherosclerotic <- JoinLayers(Integration_atherosclerotic) 运行失败
+
+dim(Integration_atherosclerotic)
+#[1] 22024 16668
+
+saveRDS(Integration_atherosclerotic,"Integration_atherosclerotic.rds"  )
+
+#################################### Do Violin Plots ######################################################
+Integration_atherosclerotic <- read_rds("Integration_atherosclerotic.rds")
+
+Integration_atherosclerotic@meta.data$celltype2 <- "Others"
+length(which(Integration_atherosclerotic@meta.data$SingleR_map_to_TAM == "16_ECMHomeoMac"    ))#2310
+table( Integration_atherosclerotic@meta.data$SingleR_map_to_TAM   )#2310
+Integration_atherosclerotic@meta.data$celltype2[which(   Integration_atherosclerotic@meta.data$SingleR_map_to_TAM == "16_ECMHomeoMac"  )] <- "16_ECMHomeoMac" 
+table(Integration_atherosclerotic@meta.data$celltype2)
+#16_ECMHomeoMac         Others 
+# 2310          14358  
+table(Integration_atherosclerotic@meta.data$celltype2, Integration_atherosclerotic@meta.data$Tissue)
+#               ascending aortic wall tissue  carotid artery atherosclerotic core (AC) carotid artery proximal adjacent (PA) coronary atherosclerotic core (AC)
+#16_ECMHomeoMac                            4                                     2302                                     0                                  4
+#Others                                 3062                                     8491                                   989                               1816
+
+Idents(Integration_atherosclerotic ) <- "Tissue"
+p=VlnPlot(Integration_atherosclerotic ,idents = "carotid artery atherosclerotic core (AC)" ,   features = c("FABP5","CD36","CD68","PLIN2","LIPA","APOE","LPL","LAMP1","TREM2","IL1B","IL10","LGALS1", "LGALS3", "CD274", "SIRPA", "HLA-DPA1", "HLA-DRB1" ),group.by = "celltype2",raster=FALSE, stack = TRUE, sort = FALSE, flip = TRUE)+ theme(legend.position = "none") + ggtitle("Atherosclerotic core") + theme(plot.margin = unit(c(0.1,0.1,0.1,1), "inches"))
+
+setwd("/Users/lucia/Documents/34.immunometabolism")
+ggsave(p,filename = "plots/Lipid_markers_of_16_ECMHomeoMac_in_Carotid_Artery_AC.jpeg",height = 10,width = 4) 
