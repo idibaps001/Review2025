e684d2ad6059dc0db889b8261e5c60764e2a069f
Lu Han <118617010+idibaps001@users.noreply.github.com>
2025-07-24 13:38:12 +0200

    Update 01.Percentage_cluster16.R

--- 03.Plots/01.Percentage_cluster16.R
+++ 03.Plots/01.Percentage_cluster16.R
@@ -21,17 +21,17 @@ df
 #4 Carotid artery atherosclerotic core (AC)                 2302         8491      21.33
 
 p = ggplot(df, aes(x = Tissue, y = Percentage, fill = Tissue)) +
-  geom_bar(stat = "identity", show.legend = TRUE) + # 显示图例
+  geom_bar(stat = "identity", show.legend = TRUE) + 
   scale_fill_manual(values = c( "#5494ce","#CEA876","#0d898a","#A47053"   )) +
   scale_y_continuous(limits = c(0, 23)) +
-  theme_bw(base_size = 16) + # 设置基本字体大小为16
+  theme_bw(base_size = 16) + 
   labs(title = "Percentage of Cluster16 across different tissues",
        x = "Tissue Type", 
        y = "Percentage (%)") +
-  theme(axis.text.x = element_blank(),  # 旋转X轴标签
-        axis.text = element_text(size = 14),  # 增加轴标签字体大小
-        axis.title = element_text(size = 16), # 增加轴标题字体大小
-        plot.title = element_text(size = 18, face = "bold"))+ # 增加标题字体大小并加粗
+  theme(axis.text.x = element_blank(),  
+        axis.text = element_text(size = 14),  
+        axis.title = element_text(size = 16), 
+        plot.title = element_text(size = 18, face = "bold"))+ 
         
   #theme(plot.margin = unit(c(0.1,0.1,0.1,1.5), "inches")) +
   geom_text(aes(label = paste0(Percentage, "%")), vjust = -0.5, size = 5)
